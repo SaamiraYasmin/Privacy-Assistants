a46867e932b4d7eb2d50d5c6fb58af3ce29ec9bb
Saamira Yasmin <75434753+SaamiraYasmin@users.noreply.github.com>
2024-12-01 15:25:03 +0530

    Add files via upload

---
+++ ml-xgboost/src/index.js
@@ -0,0 +1,7 @@
+import loadXGBoost from './loadXGBoost';
+
+var xgboostWASM = require('../dist/wasm/xgboost');
+
+// module.exports = xgboostWASM.isReady.then((l) => loadXGBoost(l));
+
+export {xgboostWASM, loadXGBoost};
